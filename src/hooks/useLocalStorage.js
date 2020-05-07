8da856e5291f5013297cd83a3cfbac3b85f3d929
tomytank <github@iamtom.com>
2020-05-07 18:26:20 +0200

    Added local storage hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,9 +1,7 @@
 import {useState} from 'react';
 
 export const useLocalStorage = key => {
-    const [value, setValue] = useState(
-        window.localStorage.getItem(key)
-        );
+    const [value, setValue] = useState(window.localStorage.getItem(key));
     const handleValueChanges = e => {
         // console.log("I'm handlveValueChanges function in useLocalStorage")
     window.localStorage.setItem(key, e.target.value);  
