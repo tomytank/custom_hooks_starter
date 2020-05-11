e6a21ebfa94d4a69347cf4b69179f50305ae216b
tomytank <github@iamtom.com>
2020-05-11 12:11:29 +0200

    Minor logic changes to setlocalstorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,17 +2,18 @@ import {useState} from 'react';
 
 export const useLocalStorage = key => {
     const [value, setValue] = useState(window.localStorage.getItem(key));
-    const handleValueChanges = e => {
+
+    const setStateAndLocalStorage = newValue => { // combining localstorage and state setting together
+        setValue(newValue); //setting state value
+        window.localStorage.setItem(key, newValue) //setting localstorage value
+    };
+    const handleValueChanges = e => {// 
         // console.log("I'm handlveValueChanges function in useLocalStorage")
-    window.localStorage.setItem(key, e.target.value);  
+    window.localStorage.setItem(key, e.target.value);
     setValue(e.target.value);
     };
 
-    const setStateAndLocalStorage = newValue => {
-        setValue(newValue);
-        window.localStorage.setItem(key, newValue)
 
-    };
     return [value, setStateAndLocalStorage, handleValueChanges];
 };
 
